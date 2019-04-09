66dde82faa5880c786ba7d12962f21e66d71b22a
Maryam <launoismaryam@gmail.com>
2019-04-09 16:08:01 +0200

    Add displayPlayMenu and displayMode methods

--- src/com/ask/maryam/menu/Menu.java
+++ src/com/ask/maryam/menu/Menu.java
@@ -14,6 +14,29 @@ public class Menu {
         System.out.println("2: Jeux du plus ou moins");
 
        selectedMenu = sc.nextInt();
+       displayPlayMenu();
+
+    }
+
+    public void displayPlayMenu(){
+        if(selectedMenu == 1) {
+            System.out.println("\n********** Bienvenue dans le jeu Mastermind **********\n");
+            displayMode();
+
+
+
+        } else if(selectedMenu == 2) {
+            System.out.println("\n********** Bienvenue dans le jeu Mastermind **********\n");
+            displayMode();
+        }
+
+    }
+
+    public void displayMode() {
+        System.out.println("Chosissez un mode");
+        System.out.println("1: Challenger");
+        System.out.println("2: Defenser");
+        System.out.println("3: Duel");
     }
 
 
