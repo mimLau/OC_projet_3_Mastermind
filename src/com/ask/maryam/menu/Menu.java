624700c617ea45cf1c71d5aa7eff9c395d3db111
Maryam <launoismaryam@gmail.com>
2019-04-09 19:05:08 +0200

    put selectedMenu at 0 in displayMode method. Add luanchPlayMethod

--- src/com/ask/maryam/menu/Menu.java
+++ src/com/ask/maryam/menu/Menu.java
@@ -1,45 +1,106 @@
 package com.ask.maryam.menu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
 
     Scanner sc = new Scanner(System.in);
-    int selectedMenu;
+    private int selectedMenu;
+    private boolean mastermindPlay;
+    private boolean moreAndLessPlay;
+
 
     public void displayPrincipalMenu(){
 
-        System.out.println("\n************ Menu principal **********\n");
-        System.out.println("1: Mastermind");
-        System.out.println("2: Jeux du plus ou moins");
+        do {
+            System.out.println("\n************ Menu principal **********\n");
+            System.out.println("1: Mastermind");
+            System.out.println("2: Jeux du plus ou moins");
 
-       selectedMenu = sc.nextInt();
-       displayPlayMenu();
+            try {
+                selectedMenu = sc.nextInt();
+            } catch (InputMismatchException e){
+                sc.next();
+            }
 
+        } while(selectedMenu != 1 && selectedMenu !=2);
+        displayPlayMenu();
     }
 
+
+
     public void displayPlayMenu(){
-        if(selectedMenu == 1) {
-            System.out.println("\n********** Bienvenue dans le jeu Mastermind **********\n");
-            displayMode();
 
-
-
-        } else if(selectedMenu == 2) {
-            System.out.println("\n********** Bienvenue dans le jeu Mastermind **********\n");
-            displayMode();
-        }
-
+             if(selectedMenu == 1) {
+                 System.out.println("\n********** Bienvenue dans le jeu Mastermind **********\n");
+                 mastermindPlay = true;
+             } else if(selectedMenu == 2) {
+                 System.out.println("\n********** Bienvenue dans le jeu du plus ou moins **********\n");
+                 moreAndLessPlay = true;
+             }
+        displayMode();
     }
 
     public void displayMode() {
-        System.out.println("Chosissez un mode");
-        System.out.println("1: Challenger");
-        System.out.println("2: Defenser");
-        System.out.println("3: Duel");
+        do {
+            selectedMenu =0;
+            System.out.println("Choisissez un mode");
+            System.out.println("1: Challenger");
+            System.out.println("2: Defenser");
+            System.out.println("3: Duel");
+
+            try {
+                selectedMenu = sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.next();
+            }
+            launchPlay();
+
+        }while(selectedMenu != 1 && selectedMenu != 2 && selectedMenu != 3);
+
     }
 
+    public void launchPlay(){
 
+        if(mastermindPlay) {
 
+            switch (selectedMenu){
+                case 1:
+                    //TODO create mastermmind class with challengerMode attribut.
+                    System.out.println("Vous avez choisi de jouer à Mastermind en mode challenger.");
+                    break;
 
+                case 2:
+                    //TODO create Mastermind class with defenderMode attribut:
+                    System.out.println("Vous avez choisi de jouer à Mastermind en mode defenseur.");
+                    break;
+
+                case 3:
+                    //TODO create Mastermind class with dualMode attribut:
+                    System.out.println("Vous avez choisi de jouer à Mastermind en mode duel.");
+                    break;
+            }
+
+        } else if (moreAndLessPlay){
+
+            switch (selectedMenu){
+                case 1:
+                    //TODO create MoreAndLessPlay class class with challengerMode attribut.
+                    System.out.println("Vous avez choisi de jouer au jeu du plus ou moins en mode challenger.");
+                    break;
+
+                case 2:
+                    //TODO create MoreAndLessPlay class with defenderMode attribut:
+                    System.out.println("Vous avez choisi de jouer au jeu du plus ou moins en mode defenseur.");
+                    break;
+
+                case 3:
+                    //TODO create MoreAndLessPlay class with dualMode attribut:
+                    System.out.println("Vous avez choisi de jouer au jeu du plus ou moins en mode duel.");
+                    break;
+            }
+        }
+
+    }
 }
