aef0b5ca43f400bb8586c51caa9a78d0b06572d1
Maryam <launoismaryam@gmail.com>
2019-04-09 19:43:00 +0200

    Add getter

--- src/com/ask/maryam/play/ChallengerMode.java
+++ src/com/ask/maryam/play/ChallengerMode.java
@@ -3,4 +3,12 @@ package com.ask.maryam.play;
 public class ChallengerMode extends Mode{
     private int [] playerProposedNbTable;
     private int [] computerScretNbTable;
+
+    public int[] getPlayerProposedNbTable() {
+        return playerProposedNbTable;
+    }
+
+    public int[] getComputerScretNbTable() {
+        return computerScretNbTable;
+    }
 }
