4358f97fda0335960121cccae6fbacc3fefa0440
Maryam <launoismaryam@gmail.com>
2019-04-09 15:43:39 +0200

    Creation de la classe displayPincipalMenu

--- src/com/ask/maryam/menu/Menu.java
+++ src/com/ask/maryam/menu/Menu.java
@@ -1,4 +1,22 @@
 package com.ask.maryam.menu;
 
+import java.util.Scanner;
+
 public class Menu {
+
+    Scanner sc = new Scanner(System.in);
+    int selectedMenu;
+
+    public void displayPrincipalMenu(){
+
+        System.out.println("\n************ Menu principal **********\n");
+        System.out.println("1: Mastermind");
+        System.out.println("2: Jeux du plus ou moins");
+
+       selectedMenu = sc.nextInt();
+    }
+
+
+
+
 }
