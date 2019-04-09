c31c969b2d51d9c72e0b962226e52c4c070780e9
Maryam <launoismaryam@gmail.com>
2019-04-09 15:44:35 +0200

    Add Menu object

--- src/com/ask/maryam/Main.java
+++ src/com/ask/maryam/Main.java
@@ -1,4 +1,12 @@
 package com.ask.maryam;
 
+import com.ask.maryam.menu.Menu;
+
 public class Main {
+
+    public static void main(String [] args){
+
+        Menu menu = new Menu();
+        menu.displayPrincipalMenu();
+    }
 }
