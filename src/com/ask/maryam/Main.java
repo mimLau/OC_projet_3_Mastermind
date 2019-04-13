4be33b7f38b15bdf47510c2b2466715c1d142e0a
Maryam <launoismaryam@gmail.com>
2019-04-13 16:45:30 +0200

    Some test for retrieving parameters

--- src/com/ask/maryam/Main.java
+++ src/com/ask/maryam/Main.java
@@ -1,12 +1,25 @@
 package com.ask.maryam;
 
 import com.ask.maryam.menu.Menu;
+import com.ask.maryam.parameters.Parameters;
 
 public class Main {
 
     public static void main(String [] args){
 
-        Menu menu = new Menu();
-        menu.displayPrincipalMenu();
+        /*Menu menu = new Menu();
+        menu.displayPrincipalMenu();*/
+
+        Parameters params = new Parameters();
+        params.retrieveProperties();
+
+        System.out.println("Nombre de cases de la combinaison secrète: " + params.getSecretNbSize());
+        System.out.println("Nombre d'essais possible: " + params.getTrialNbMax());
+        System.out.println("Nombre de chiffre utilisable  " + params.getMaxUsableDigit());
+
+
+        params.setDevMode(Boolean.valueOf(args[0]).booleanValue());
+
+        System.out.println("Mode développeur " + params.isDevMode());
     }
 }
