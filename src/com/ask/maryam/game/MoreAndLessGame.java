a13bba29871849bb85a002b28308a4f532942ae5
Maryam <launoismaryam@gmail.com>
2019-04-13 12:07:24 +0200

    Override interfaces methods

--- src/com/ask/maryam/game/MoreAndLessGame.java
+++ src/com/ask/maryam/game/MoreAndLessGame.java
@@ -1,8 +1,35 @@
 package com.ask.maryam.game;
 
+import com.ask.maryam.mode.Mode;
+
 public class MoreAndLessGame implements Game {
+    private String [] moreAndLessTable;
+    private Mode mode;
+    //private Parameters pram;
+
+
     @Override
-    public void startGameMessage() {
+    public void startWelcomeMessage() {
         System.out.println("\n********** Bienvenue dans le jeu du plus ou moins **********\n");
+    }
+
+    @Override
+    public void compareNb() {
+
+    }
+
+    @Override
+    public String displayResult() {
+        return null;
+    }
+
+    @Override
+    public boolean isGameFinisched() {
+        return false;
+    }
+
+    public String []  displayMoreAndLessTable(){
+
+        return null;
     }
 }
