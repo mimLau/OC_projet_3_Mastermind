fa73f5c69338b7cc1486a5eeafb0ce5b63fa572b
Maryam <launoismaryam@gmail.com>
2019-04-13 16:56:05 +0200

    Add parseNbInTable method

--- src/com/ask/maryam/mode/Mode.java
+++ src/com/ask/maryam/mode/Mode.java
@@ -1,9 +1,11 @@
 package com.ask.maryam.mode;
 
-import com.ask.maryam.players.ComputerPlayer;
 import com.ask.maryam.players.Player;
 
 public class Mode {
     private Player player;
-    private ComputerPlayer computer;
+
+    public int [] parseNbInTable(int number){
+        return null;
+    }
 }
