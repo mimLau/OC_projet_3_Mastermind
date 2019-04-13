118535cac8cca24fffa91e71dd58a894cf7332d0
Maryam <launoismaryam@gmail.com>
2019-04-13 12:05:36 +0200

    Add somme attributes

--- src/com/ask/maryam/game/Game.java
+++ src/com/ask/maryam/game/Game.java
@@ -4,5 +4,9 @@ public interface  Game {
      /*int trialNb;
      boolean gameOver;*/
 
-    public  void startWelcomeMessage();
+    public void startWelcomeMessage();
+    public void compareNb();
+    public String displayResult();
+    public boolean isGameFinisched();
+
 }
