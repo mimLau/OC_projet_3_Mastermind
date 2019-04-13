f85f6e9716c6663cd010848dc3922b6e94f170fd
Maryam <launoismaryam@gmail.com>
2019-04-13 16:57:41 +0200

    Add somes attributes

---
+++ src/com/ask/maryam/mode/DualMode.java
@@ -0,0 +1,9 @@
+package com.ask.maryam.mode;
+
+public class DualMode extends Mode{
+
+    private int [] playerSecretNbTable;
+    private int [] playerProposedNbTable;
+    private int [] computerSecretNbTable;
+    private  int[] computerProposedNbTable;
+ }
