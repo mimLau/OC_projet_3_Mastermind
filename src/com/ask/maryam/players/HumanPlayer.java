725c715ea53df8b691616818af710ad81e8fcf46
Maryam <launoismaryam@gmail.com>
2019-04-13 12:09:51 +0200

    Add attribute name

---
+++ src/com/ask/maryam/players/HumanPlayer.java
@@ -0,0 +1,17 @@
+package com.ask.maryam.players;
+
+import com.ask.maryam.players.Player;
+
+public class HumanPlayer extends Player {
+    private int playerSecretNb;
+    private int playerProposedNb;
+    private String Name;
+
+    public int getSecretNb(){
+
+
+        return 5;
+    }
+
+
+}
