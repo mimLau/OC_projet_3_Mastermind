c85fd02eeb7b5b99941c7650526261fab9a20e5e
Maryam <launoismaryam@gmail.com>
2019-04-13 12:12:38 +0200

    Delete computer attribute

---
+++ src/com/ask/maryam/mode/Mode.java
@@ -0,0 +1,9 @@
+package com.ask.maryam.mode;
+
+import com.ask.maryam.players.ComputerPlayer;
+import com.ask.maryam.players.Player;
+
+public class Mode {
+    private Player player;
+    private ComputerPlayer computer;
+}
