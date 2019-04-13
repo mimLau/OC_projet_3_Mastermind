593d263e0cf478bb2359c828ad9c1f9d39c23c30
Maryam <launoismaryam@gmail.com>
2019-04-13 11:04:31 +0200

    Add starGame method. Implement interface Game

---
+++ src/com/ask/maryam/game/MoreAndLessGame.java
@@ -0,0 +1,8 @@
+package com.ask.maryam.game;
+
+public class MoreAndLessGame implements Game {
+    @Override
+    public void startGame() {
+        System.out.println("Bienvenue dans le jeu du plus ou moin.");
+    }
+}
