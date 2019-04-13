4af39427a255947e3200014c083a55d7662d25d0
Maryam <launoismaryam@gmail.com>
2019-04-13 11:16:39 +0200

    Change name of startGame method into startGameMessage

--- src/com/ask/maryam/game/MoreAndLessGame.java
+++ src/com/ask/maryam/game/MoreAndLessGame.java
@@ -2,7 +2,7 @@ package com.ask.maryam.game;
 
 public class MoreAndLessGame implements Game {
     @Override
-    public void startGame() {
-        System.out.println("Bienvenue dans le jeu du plus ou moin.");
+    public void startGameMessage() {
+        System.out.println("\n********** Bienvenue dans le jeu du plus ou moins **********\n");
     }
 }
