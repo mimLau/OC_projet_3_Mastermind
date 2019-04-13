5e3a36c47798d10caddd930e80f15317aa6774ba
Maryam <launoismaryam@gmail.com>
2019-04-13 11:02:58 +0200

    Add startGame method. No Mastermind implement a interface

--- src/com/ask/maryam/game/MastermindGame.java
+++ src/com/ask/maryam/game/MastermindGame.java
@@ -2,14 +2,22 @@ package com.ask.maryam.game;
 
 import com.ask.maryam.mode.Mode;
 
-public class MastermindGame extends Game{
+public class MastermindGame implements Game{
    private int nbOfGoodPlace;
    private int nbOfPresentNb;
    private Mode mode;
+
+
+   public void startGame() {
+       System.out.println("Bienvenue dans le jeu Mastermind.");
+   }
+
+
+
    //PlayParameters playParams;
 
 
-    public MastermindGame(Mode mode) {
+    /*public MastermindGame(Mode mode) {
         this.mode = mode;
-    }
+    }*/
 }
