e84677d3d526f2afee85c0ebbd26da55cb8cf154
Maryam <launoismaryam@gmail.com>
2019-04-14 19:16:15 +0200

    Complete methode getProposedNb with verification of parameters. Add maximalUsableDigitOk methode wich return false if proposed numbers digit are greater than maxUsableDigit param.

--- src/com/ask/maryam/players/HumanPlayer.java
+++ src/com/ask/maryam/players/HumanPlayer.java
@@ -5,18 +5,55 @@ import com.ask.maryam.players.Player;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class HumanPlayer extends Player {
     private String playerSecretNb;
     private String playerProposedNb;
     private String Name;
     private Parameters params = new Parameters();
+    boolean isMaximalDigitOk = true;
 
     Scanner sc = new Scanner(System.in);
 
-    public String getProposedNb(){
-        //int maxUsableDigit = params.getMaxUsableDigit();
-        playerProposedNb = sc.next();
+    public HumanPlayer() {
+    }
+
+    public String getPlayerProposedNb() {
         return playerProposedNb;
+    }
+
+    public String getProposedNb(){
+        StringBuilder stbuild = new StringBuilder();
+        int secretNbSize = params.getSecretNbSize();
+        int maxUsableDigit = params.getMaxUsableDigit();
+
+        do{
+            playerProposedNb = sc.next();
+
+            isMaximalDigitOk = maximalUsableDigitOk(playerProposedNb, maxUsableDigit);
+
+            if(playerProposedNb.length() > secretNbSize)
+                System.out.println("Choisissez un nombre à " + secretNbSize + " chiffres.");
+            if(!isMaximalDigitOk)
+                System.out.println("Choisissez un nombre avec des chiffres compris entre 0 et " + maxUsableDigit + ".");
+
+        }while(playerProposedNb.length() > secretNbSize || !isMaximalDigitOk);
+
+        return playerProposedNb;
+    }
+
+    private boolean maximalUsableDigitOk(String playerNumber, int maxUsableDigit){
+        for(int i=0; i<playerNumber.length(); i++){
+
+            if(Character.getNumericValue(playerNumber.charAt(i)) <= maxUsableDigit){
+                isMaximalDigitOk= true;
+            }
+            else {
+                isMaximalDigitOk= false;
+            }
+        }
+        return isMaximalDigitOk;
     }
 
     public String getSecretNb(){
