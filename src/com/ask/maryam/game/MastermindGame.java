dce07a9db0f8303f74261124129af5e55eb112ec
Maryam <launoismaryam@gmail.com>
2019-04-14 21:13:55 +0200

    Fill goodPlace and goodNumber method.Create answerMessage method. Add boucle do while in startPlaying methode.

--- src/com/ask/maryam/game/MastermindGame.java
+++ src/com/ask/maryam/game/MastermindGame.java
@@ -9,10 +9,14 @@ import com.ask.maryam.parameters.Parameters;
 import java.util.List;
 
 public class MastermindGame implements Game{
-   private int nbOfGoodPlace;
-   private int nbOfPresentNb;
+   private int nbOfGoodPlace = 0;
+   private int nbOfPresentNb = 0;
    private Mode mode;
    private Parameters params= new Parameters();
+
+   int secretNbSize = params.getSecretNbSize();
+   int maxUsableDigit = params.getMaxUsableDigit();
+   int trialNbMax = params.getTrialNbMax();
 
    public MastermindGame() {}
 
@@ -20,40 +24,94 @@ public class MastermindGame implements Game{
       this.mode = mode;
    }
 
-   public int goodPlace(){
-      return 0;
+   /**
+    *
+    * @param secretNb
+    * @param proposedNb
+    * @return
+    */
+   public int goodPlace(List<Integer> secretNb, List<Integer> proposedNb){
+      for(int i=0; i<secretNbSize; i++){
+         if(secretNb.get(i) == proposedNb.get(i)){
+            nbOfGoodPlace++;
+         }
+      }
+      return nbOfGoodPlace;
    }
 
-   public int goodnumber(){
-      return 0;
+   /**
+    *
+    * @param secretNb
+    * @param proposedNb
+    * @return
+    */
+   public int goodNumber(List<Integer> secretNb, List<Integer> proposedNb){
+      for(int i=0; i<secretNbSize; i++){
+            if(secretNb.contains(proposedNb.get(i))&& proposedNb.get(i) != secretNb.get(i)){
+            nbOfPresentNb++;
+         }
+      }
+      return nbOfPresentNb;
    }
 
+   /**
+    *
+    */
    public void startWelcomeMessage() {
        System.out.println("\n********** Bienvenue dans le jeu Mastermind **********\n");
    }
 
+   /**
+    *
+    * @param challMode
+    */
    @Override
    public void startPlaying(ChallengerMode challMode) {
 
-      int secretNbSize = params.getSecretNbSize();
-      int getMaxUsableDigit = params.getMaxUsableDigit();
-
       System.out.println("Vous avez choisi le mode CHALLENGER, vous devez deviner le nombre secret de l'ordinateur.");
       System.out.println("Tapez un nombre de " + secretNbSize + " chiffres.");
-      System.out.println("Vous pouvez choisir les chiffres allant de 0 à " + getMaxUsableDigit +".");
-
-
+      System.out.println("Vous pouvez choisir les chiffres allant de 0 à " + maxUsableDigit +".");
       List<Integer> computerSecretNbList = challMode.putComputerSecretNbInList();
-      List<Integer> playerProposedTable = challMode.putPlayerProposedNInList();
-      System.out.println("Proposition : " + challMode.getPlayer().getPlayerProposedNb() + " -> ");
+      do {
 
+         List<Integer> playerProposedNbList = challMode.putPlayerProposedNbInList();
 
-      System.out.println(playerProposedTable);
-     System.out.println(computerSecretNbList);
+         nbOfGoodPlace = goodPlace(computerSecretNbList, playerProposedNbList);
+         nbOfPresentNb = goodNumber(computerSecretNbList, playerProposedNbList);
+
+         answerMessage(nbOfGoodPlace, nbOfPresentNb, challMode);
+
+         trialNbMax--;
+
+         System.out.println(playerProposedNbList);
+         System.out.println(computerSecretNbList);
+
+      }while(trialNbMax!=0 || nbOfGoodPlace == secretNbSize);
 
       // need computer secret nb table
-      // need scann method for retrieving player proposed number transformed in a table
+      // need scan method for retrieving player proposed number transformed in a table
       // who tour is it
+   }
+
+   /**
+    *
+    * @param nbOfGoodPlace
+    * @param nbOfPresentNb
+    * @param challMode
+    */
+   public void answerMessage(int nbOfGoodPlace, int nbOfPresentNb, ChallengerMode challMode ){
+      if(nbOfGoodPlace > 1 && nbOfPresentNb > 1)
+         System.out.println("Proposition : " + challMode.getPlayer().getPlayerProposedNb() + " -> Réponse : "
+                                             + nbOfPresentNb + " présents, " + nbOfGoodPlace + " bien placés.");
+      else if(nbOfGoodPlace > 1 && nbOfPresentNb < 1)
+         System.out.println("Proposition : " + challMode.getPlayer().getPlayerProposedNb() + " -> Réponse : "
+                                             + nbOfPresentNb + " présent, " + nbOfGoodPlace + " bien placés.");
+      else if(nbOfPresentNb > 1 && nbOfGoodPlace < 1)
+         System.out.println("Proposition : " + challMode.getPlayer().getPlayerProposedNb() + " -> Réponse : "
+                 + nbOfPresentNb + " présents, " + nbOfGoodPlace + " bien placé.");
+      else
+         System.out.println("Proposition : " + challMode.getPlayer().getPlayerProposedNb() + " -> Réponse : "
+                 + nbOfPresentNb + " présent, " + nbOfGoodPlace + " bien placé.");
    }
 
    @Override
@@ -67,8 +125,7 @@ public class MastermindGame implements Game{
    }
 
    @Override
-   public void compareNb() {
-
+   public void compareNb(List<Integer> secretNb, List<Integer> proposedNb) {
    }
 
    @Override
