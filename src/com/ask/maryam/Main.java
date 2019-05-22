ce7de6487acd4fda0ee884d27a1b9297d4f6d7b1
Maryam <launoismaryam@gmail.com>
2019-05-22 15:44:06 +0200

    Add Main class

--- src/com/ask/maryam/Main.java
+++ src/com/ask/maryam/Main.java
@@ -7,17 +7,29 @@ public class Main {
 
     public static void main(String [] args){
 
-        Parameters params = new Parameters();
-        try {
-            if(args[0] != null){
-                params.setDevMode(Boolean.parseBoolean(args[0]));
-            }
-        }catch (ArrayIndexOutOfBoundsException configParamNull){
-            params.setDevMode(false);
+        /*
+            Set all the parameters which are in the config.properties file.
+         */
+        Parameters params = Parameters.INSTANCE;
+
+        params.setSecretNbSize();
+        params.setTrialNbMax();
+        params.setMaxUsableDigit();
+
+        /*
+            Verify if the argument isDev in the "run config" exits, if true we use it, if not, we set the dev mode
+            from parameters which are in the config.properties file.
+         */
+        if(args.length != 0){
+            params.setDevMode(Boolean.parseBoolean(args[0]));
+        } else {
+            params.setDevMode();
         }
 
-        Menu menu = new Menu(params);
-        menu.displayPrincipalMenu();
-
+        //Launch the menu only if all the parameters are configured in the config.properties file.
+        if(params.getMaxUsableDigit()!=0 && params.getTrialNbMax()!=0 && params.getSecretNbSize()!=0) {
+            Menu menu = new Menu();
+            menu.displayPrincipalMenu();
+        }
     }
 }
